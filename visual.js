4bfd8c7f2161ef5536a14ea178894b62bc925745
austramarta <austramarta@users.noreply.github.com>
2021-01-19 13:45:48 +0200

    Resolving pull request comments from Karlis

--- visual.js
+++ visual.js
@@ -1,36 +1,37 @@
-//DEFINING NECESSARY GLOBAL VARIABLES
-//define board size
+// DEFINING NECESSARY GLOBAL VARIABLES.
+// Define board size.
 let chessBoardSize = 600;
 let squaresInRow = 8;
 
-//define canvas
+// Define canvas.
 const canvas = document.getElementById("chess-board");
 const ctx = canvas.getContext("2d");
-//make canvas size dependent on the board size
+// Make canvas size dependent on the board size.
 canvas.width = chessBoardSize;
 canvas.height = chessBoardSize;
 
-//define square size and amount for a particular chess board
+// Define square size and amount.
 let squareSize = chessBoardSize / squaresInRow;
 let totalSquares = Math.pow(squaresInRow, 2);
 
+// Define starting positions from logic.js.
 let startingPositions = getStartingPositions();
 
 function drawChessGame() {
-    //function that draws chess board
+    // Draws chess board.
     drawBoard();
-    //draw initial pieces for the beginning of the game
+    // Draw pieces for the beginning of the game.
     drawInitialPieces();
-    //draw chess pieces
+    // Draw chess pieces.
     // drawPiece();
 
 
-    //catch when clicking on a piece
+    // Catch e when clicking on a piece.
     canvas.addEventListener("click", clickOnPiece);
 }
 
 function drawBoard() {
-    //draw the board using a nested loop
+    // Draw the board using a nested loop.
     for (let x = 0; x < totalSquares; x++) {
         for (let y = 0; y < totalSquares; y++) {
             if ((x + y) % 2 == 0) {
@@ -45,13 +46,13 @@ function drawBoard() {
     }
 }
 
-
 function drawInitialPieces() {
     startingPositions.forEach(function (piece) {
         drawPiece(piece);
     })
 }
 
+// Getting mouse position when clicked.
 function getMousePos(canvas, e) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -61,29 +62,23 @@ function getMousePos(canvas, e) {
 }
 
 function clickOnPiece(e) {
-    console.log("Hello Chess2");
     let mousePos = getMousePos(canvas, e);
     console.log(mousePos);
-
-
     console.log(Math.ceil(mousePos.x / squareSize));
     console.log(Math.ceil(mousePos.y / squareSize));
 }
-
 
 function drawPiece(piece) {
     var img = new window.Image();
     img.addEventListener("load", function () {
         ctx.drawImage(img, (piece.x - 1) * squareSize, (piece.y - 1) * squareSize);
     });
-
     const pieceFileNames = {
         "Q": "Queen",
         "K": "King",
         "H": "Knight",
         "R": "Rook"
     }
-
     //logic to make which piece is necessary
     let fileName = "assets/";
     if (piece.isWhite) {
@@ -91,14 +86,6 @@ function drawPiece(piece) {
     } else {
         fileName = fileName + "black";
     }
-
     fileName = fileName + pieceFileNames[piece.type]
     img.setAttribute("src", fileName + ".png");
-
 }
-
-
-
-
-
-
